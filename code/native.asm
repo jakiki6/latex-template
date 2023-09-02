1d2d4f5555a3368d54b42d511a11c000f93c4395
Jakob Kirsch <jakob.kirsch@web.de>
2023-09-03 01:52:38 +0200

    improve test code

--- code/native.asm
+++ code/native.asm
@@ -1,16 +1,10 @@
 align 16
 global native_rdtsc
 native_rdtsc:
-        push rax
 	push rbx
-	push rcx
-	push rdx
 	xor rax, rax
 	cpuid
-        pop rdx
-	pop rcx
 	pop rbx
-	pop rax
 
 	rdtsc
 	shl rdx, 32
@@ -18,24 +12,15 @@ native_rdtsc:
 
         push rax
 	push rbx
-	push rcx
-	push rdx
 	xor rax, rax
 	cpuid
-	pop rdx
-	pop rcx
 	pop rbx
 	pop rax
 
 	ret
 
-align 16
+align 64
 global native_rdseed
 native_rdseed:
 	rdseed rax
-	ret
-
-align 16
-global native_ret
-native_ret:
 	ret
--- code/test_rdseed.c
+++ code/test_rdseed.c
@@ -3,25 +3,18 @@
 
 #include "native.h"
 
-#define ROUNDS 1000
+#define ROUNDS 0x100000
 
 int main() {
-    uint64_t base_cycles = 0;
     uint64_t test_cycles = 0;
-
-    for (int i = 0; i < ROUNDS; i++) {
-        uint64_t start = native_rdtsc();
-        native_ret(); 
-        uint64_t delta = native_rdtsc() - start;
-        base_cycles += delta;
-    }
 
     for (int i = 0; i < ROUNDS; i++) {
         uint64_t start = native_rdtsc();
         native_rdseed();
 	uint64_t delta = native_rdtsc() - start;
-	test_cycles += delta;
+
+	if (delta < 10000) test_cycles += delta;
     }
 
-    printf("average cycles/inst: %lu\n", (test_cycles - base_cycles) / ROUNDS);
+    printf("average cycles/inst: %f\n", (double) test_cycles / ROUNDS);
 }
